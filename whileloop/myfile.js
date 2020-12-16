9990eabf210d6bc5af7b871f4be2aa06d726adb2
kdaud <dkakumirizii@gmail.com>
2020-12-16 19:48:25 +0300

    added while loop implementation

---
+++ whileloop/myfile.js
@@ -0,0 +1,16 @@
+const myState = [
+'Uganda',
+'Kenya',
+'Rwanda',
+'Tanzania',
+'Sudan',
+2008,
+'Burundi',
+'Zimbabwe'
+]
+
+let y=0;
+while (y<myState.length-1) {
+    console.log(myState[y]);
+    y++;
+}
