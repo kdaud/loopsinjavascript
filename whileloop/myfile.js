1db27c7f43a0073ad4799483ce659cc13038adb5
kdaud <dkakumirizii@gmail.com>
2020-12-16 20:06:09 +0300

    for loop api

--- whileloop/myfile.js
+++ whileloop/myfile.js
@@ -9,8 +9,23 @@ const myState = [
 'Zimbabwe'
 ]
 
-let y=0;
-while (y<myState.length-1) {
-    console.log(myState[y]);
-    y++;
+// let y=0;
+// while (y<myState.length) {
+//     console.log(myState[y]);
+//     y++;
+// }
+console.log("\n********\n");
+
+let yy=0;
+ do{
+    console.log(myState[yy]);
+    yy++;
+}while (yy<myState.length);
+
+console.log("\n^^^^^^^*****\n");
+for (let x = 0; x < myState.length; x++) {
+    const element = myState[x];
+    console.log(element);
+
+    
 }
